4e6094e704d90bfe87178d62adc28e1a6c7eb5c7
tuxuser <tuxuser360@gmail.com>
2014-06-26 16:28:09 +0200

    Add list of compress-/deletable files

--- OZMHelper/ozmhelper.cpp
+++ OZMHelper/ozmhelper.cpp
@@ -25,7 +25,7 @@ static const sectionEntry requiredFfs[] = {
   {"EnhancedFat","961578FE-B6B7-44C3-AF35-6BC705CD2B1F",TRUE},
   {"HfsPlus","4CF484CD-135F-4FDC-BAFB-1AA104B48D36",TRUE},
   {"Ozmosis","AAE65279-0761-41D1-BA13-4A3C1383603F",TRUE},
-  {"OzmosisDefaults","99F2839C-57C3-411E-ABC3-ADE5267D960D",TRUE},
+  {"OzmosisDefaults","99F2839C-57C3-411E-ABC3-ADE5267D960D",TRUE},// not required, but small
   {"SmcEmulatorKext","DADE1001-1B31-4FE4-8557-26FCEFC78275",TRUE}
 };
 
@@ -39,20 +39,50 @@ static const sectionEntry optionalFfs[] = {
   {"InjectorKext","DADE1003-1B31-4FE4-8557-26FCEFC78275", FALSE}
 };
 
+#define compressFfsCount 1
+
+static const sectionEntry compressFfs[] = {
+  {"CORE_DXE","5AE3F37E-4EAE-41AE-8240-35465B5E81EB", TRUE}
+};
+
+#define deletableFfsCount 11
+
+static const sectionEntry deletableFfs[] = {
+  {"FileSystem","93022F8C-1F09-47EF-BBB2-5814FF609DF5", FALSE},
+  {"TcpDxe","B1625D3C-9D2D-4E0D-B864-8A763EE4EC50",FALSE},
+  {"Dhcp4Dxe","8DD9176E-EE87-4F0E-8A84-3F998311F930",FALSE},
+  {"Ip4ConfigDxe","8F9296EF-2880-4659-B857-915A8901BDC8",FALSE},
+  {"Ip4Dxe","8F92960F-2880-4659-B857-915A8901BDC8",FALSE},
+  {"Mtftp4Dxe","61AFA223-8AC8-4440-9AB5-762B1BF05156",FALSE},
+  {"Udp4Dxe","10EE5462-B207-4A4F-ABD8-CB522ECAA3A4",FALSE},
+  {"Dhcp6Dxe","8DD9176D-EE87-4F0E-8A84-3F998311F930",FALSE},
+  {"Ip6Dxe","8F92960E-2880-4659-B857-915A8901BDC8",FALSE},
+  {"Mtftp6Dxe","61AFA251-8AC8-4440-9AB5-762B1BF05156",FALSE},
+  {"Udp6Dxe","10EE54AE-B207-4A4F-ABD8-CB522ECAA3A4",FALSE}
+};
+
 static const sectionEntry amiBoardSection = {
   "AmiBoardInfo","9F3A0016-AE55-4288-829D-D22FD344C347", TRUE
 };
 
+static QList<sectionEntry> occupyFfs;
 static QList<sectionEntry> OzmFfs;
 
 OZMHelper::OZMHelper(QObject *parent) :
     QObject(parent)
 {
+    /* how to store those initially */
     for(int i = 0; i < requiredFfsCount; i++) {
         OzmFfs.append(requiredFfs[i]);
     }
     for(int i = 0; i < optionalFfsCount; i++) {
         OzmFfs.append(optionalFfs[i]);
+    }
+    for(int i = 0; i < compressFfsCount; i++) {
+        occupyFfs.append(compressFfs[i]);
+    }
+    for(int i = 0; i < deletableFfsCount; i++) {
+        occupyFfs.append(deletableFfs[i]);
     }
 }
 
@@ -417,6 +447,7 @@ UINT8 OZMHelper::OZMCreate(QString inputfile, QString outputfile, QString inputF
     }
 
     out.clear();
+    printf("Reconstructing final image...\n");
     ret = fu->reconstructImageFile(out);
     if(ret) {
         printf("ERROR: Image exploded.. please provide fewer files!\n");
@@ -428,6 +459,7 @@ UINT8 OZMHelper::OZMCreate(QString inputfile, QString outputfile, QString inputF
         printf("ERROR: Writing patched BIOS to '%s' failed!\n", qPrintable(outputfile));
         return ret;
     }
+    printf("Bios successfully saved to '%s'\n",qPrintable(outputfile));
 
     return ERR_SUCCESS;
 }
