1987406c987189b555861c2f1d52fce17f4822ec
tuxuser <tuxuser360@gmail.com>
2014-07-12 00:25:29 +0200

    Fixing MacOS buildscript

--- OZMTool/build_macos.sh
+++ OZMTool/build_macos.sh
@@ -5,7 +5,11 @@ BUILD_DIR=${BASE_DIR}/MacOS
 RELEASE_DIR=${BUILD_DIR}/release
 FRAMEWORK_DIR=${RELEASE_DIR}/Frameworks
 EXEC_NAME=OZMTool
-FRAMEWORK=QtCore.framework
+FRAMEWORK_CORE=QtCore.framework
+FRAMEWORK_XML=QtXml.framework
+CORE_NAME=QtCore
+XML_NAME=QtXml
+
 
 QT_VER=5
 QTPATH=${QT_ENV}
@@ -41,17 +45,29 @@ mkdir ${RELEASE_DIR}
 cp ${BUILD_DIR}/${EXEC_NAME} ${RELEASE_DIR}
 cp ${BASE_DIR}/README ${RELEASE_DIR}
 mkdir ${FRAMEWORK_DIR}
-cp -R ${QT_LIB_DIR}/${FRAMEWORK} ${FRAMEWORK_DIR}
+cp -R ${QT_LIB_DIR}/${FRAMEWORK_CORE} ${FRAMEWORK_DIR}
+cp -R ${QT_LIB_DIR}/${FRAMEWORK_XML} ${FRAMEWORK_DIR}
 
 echo "Setting up proper links..."
 # Set identification name
-${INSTALL_NAME_TOOL} -id @executable_path/Frameworks/${FRAMEWORK}/Versions/${QT_VER}/QtCore \
-       ${FRAMEWORK_DIR}/${FRAMEWORK}/Versions/${QT_VER}/QtCore
+${INSTALL_NAME_TOOL} -id @executable_path/Frameworks/${FRAMEWORK_CORE}/Versions/${QT_VER}/${CORE_NAME} \
+       ${FRAMEWORK_DIR}/${FRAMEWORK_CORE}/Versions/${QT_VER}/${CORE_NAME}
+
+${INSTALL_NAME_TOOL} -id @executable_path/Frameworks/${FRAMEWORK_XML}/Versions/${QT_VER}/${XML_NAME} \
+       ${FRAMEWORK_DIR}/${FRAMEWORK_XML}/Versions/${QT_VER}/${XML_NAME}
 
 # Set path
-${INSTALL_NAME_TOOL} -change ${QT_LIB_DIR}/${FRAMEWORK}/Versions/${QT_VER}/QtCore \
-        @executable_path/Frameworks/${FRAMEWORK}/Versions/${QT_VER}/QtCore \
+${INSTALL_NAME_TOOL} -change ${QT_LIB_DIR}/${FRAMEWORK_CORE}/Versions/${QT_VER}/${CORE_NAME} \
+        @executable_path/Frameworks/${FRAMEWORK_CORE}/Versions/${QT_VER}/${CORE_NAME} \
         ${RELEASE_DIR}/${EXEC_NAME}
+
+${INSTALL_NAME_TOOL} -change ${QT_LIB_DIR}/${FRAMEWORK_XML}/Versions/${QT_VER}/${XML_NAME} \
+        @executable_path/Frameworks/${FRAMEWORK_XML}/Versions/${QT_VER}/${XML_NAME} \
+        ${RELEASE_DIR}/${EXEC_NAME}
+
+${INSTALL_NAME_TOOL} -change ${QT_LIB_DIR}/${FRAMEWORK_CORE}/Versions/${QT_VER}/${CORE_NAME} \
+        @executable_path/Frameworks/${FRAMEWORK_CORE}/Versions/${QT_VER}/${CORE_NAME} \
+        ${FRAMEWORK_DIR}/${FRAMEWORK_XML}/Versions/${QT_VER}/${XML_NAME}
 
 echo "Packing it up..."
 cd ${RELEASE_DIR}
