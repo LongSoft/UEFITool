a5ef2876de41882b4189d0478c4dd66384fa5d38
tuxuser <tuxuser360@gmail.com>
2014-07-20 00:28:02 +0200

    ..if done properly

--- OZMTool/ozmtool.cpp
+++ OZMTool/ozmtool.cpp
@@ -484,7 +484,7 @@ UINT8 OZMTool::OZMCreate(QString inputfile, QString outputfile, QString inputFFS
             return ret;
         }
 
-        printf(" * Current file: '%s'...\n", currFFS.fileName());
+        printf(" * Current file: '%s'...\n", qPrintable(currFFS.fileName()));
 
         ret = fu->injectFile(ffs);
         if (ret) {
